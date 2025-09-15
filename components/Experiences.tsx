917d9b9041aa0c2f84b24a26ba89b193a49608a7
Omar <elmanssouriomar@gmail.com>
2025-09-15 16:08:14 +0100

    feat(ui): add Experiences component with moving border animation

---
+++ components/Experiences.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { experiences } from "@/data";
+import { Card } from "./ui/MovingBorder";
+
+const Experience = () => {
+  return (
+    <section id="experiences" className="py-20">
+      <h1 className="font-bold text-3xl md:text-5xl text-center">
+        My <span className="text-purple">work experience</span>
+      </h1>
+
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {experiences.map((card) => (
+          <Card
+            key={card.id}
+            //   random duration will be fun , I think , may be not
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            style={{
+              // add this border radius to make it more rounded so that the moving border is more realistic
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
